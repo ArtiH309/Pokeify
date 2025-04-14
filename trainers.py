dfa5be5739a8037a3132fdad92e16fae490a1132
nafisuddinn <155024278+nafisuddinn@users.noreply.github.com>
2025-04-14 01:28:31 -0400

    Add files via upload

---
+++ trainers.py
@@ -0,0 +1,8 @@
+character_sprites = [
+            "https://archives.bulbagarden.net/media/upload/f/fd/Spr_HGSS_Blue.png",
+            "https://archives.bulbagarden.net/media/upload/e/e8/Spr_HGSS_Red.png",
+            "https://archives.bulbagarden.net/media/upload/d/d5/Spr_Pt_Barry.png",
+            "https://archives.bulbagarden.net/media/upload/8/83/Spr_B2W2_Cynthia.png",
+            "https://archives.bulbagarden.net/media/upload/c/cd/Spr_B2W2_Nate.png",
+            "https://archives.bulbagarden.net/media/upload/b/b7/Spr_B2W2_Hilda_2.png"
+        ]
